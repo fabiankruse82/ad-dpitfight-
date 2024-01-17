19874c6752a1d2f7d3515264c11da6d23463bf00
markus <markusteumel@gmail.com>
2024-01-17 11:00:54 +0100

    rudimentary hit check

--- combat.js
+++ combat.js
@@ -1,5 +1,21 @@
 /* Combat Code */ 
 
+//Test Character
+
+const player = {
+    name: "Guy",
+    health: 20,
+    armor: 10
+}
+
+const monster = {
+    name: "Goblin",
+    health: 20,
+    armor: 5
+}
+
+
+//Die Rolls
 
 function roll1D4() {
     return Math.floor(Math.random() * 4 + 1)
@@ -24,6 +40,21 @@ function roll1D12() {
 function roll1D20() {
     return Math.floor(Math.random() * 20 + 1)
 }
+
+/* Roll Checks */
+function thatHitMeQ(init, target, hitRoll, dmgRoll) {
+    let rollResult = hitRoll()
+    if (target.armor < rollResult) {
+        let hitResult = dmgRoll()
+        target.health - hitResult
+        return `${init.name} hit ${target.name} and did ${hitResult} Damage`
+    }else {
+        return "Miss"
+    }
+}
+
+console.log(thatHitMeQ(player,monster,roll1D20,roll1D10));
+
 
 console.log(roll1D4());
 console.log(roll1D6());
